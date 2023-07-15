653f13f4308a325f7e1fe227b00a7adc06873a9c
ritikchauhan <ritikchauhan67215@gmail.com.noreply.github.com>
2023-07-15 15:46:04 +0530

    react-todo-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,47 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import { Toaster } from "react-hot-toast";
-import { useContext, useEffect } from "react";
-import axios from "axios";
-import { Context, server } from "./main";
+  import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+  import Header from "./components/Header";
+  import Home from "./pages/Home";
+  import Profile from "./pages/Profile";
+  import Login from "./pages/Login";
+  import Register from "./pages/Register";
+  import { Toaster } from "react-hot-toast";
+  import { useContext, useEffect } from "react";
+  import axios from "axios";
+  import { Context, server } from "./main";
 
-function App() {
-  const { setUser, setIsAuthenticated, setLoading } = useContext(Context);
+  function App() {
+    const { setUser, setIsAuthenticated, setLoading } = useContext(Context);
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(`${server}/users/me`, {
-        withCredentials: true,
-      })
-      .then((res) => {
-        setUser(res.data.user);
-        setIsAuthenticated(true);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setUser({});
-        setIsAuthenticated(false);
-        setLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+      setLoading(true);
+      axios
+        .get(`${server}/api/v1/users/me`, {
+          withCredentials: true,
+        })
+        .then((res) => {
+          setUser(res.data.user);
+          setIsAuthenticated(true);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setUser({});
+          setIsAuthenticated(false);
+          setLoading(false);
+        });
+    }, []);
 
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
-      </Routes>
-      <Toaster />
-    </Router>
-  );
-}
+    return (
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+        <Toaster />
+      </Router>
+    );
+  }
 
-export default App;
+  export default App;
